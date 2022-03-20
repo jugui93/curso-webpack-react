b9e1d90cab4cbe33d0f915ad3e343ac680622b26
jugui93 <juanguillermo93@gmail.com>
2022-03-19 19:17:52 -0500

    arreglo terser

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require ('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
